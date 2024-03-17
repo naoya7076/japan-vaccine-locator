2f9dc607bf07c98a89363252441611e51f15c88f
naoya7076 <naoya7076@gmail.com>
2024-03-18 00:01:42 +0900

    病院を表示させるコンポーネントを作成

---
+++ app/components/hospital.tsx
@@ -0,0 +1,36 @@
+import {
+	Card,
+	CardHeader,
+	CardTitle,
+	CardContent,
+	CardDescription,
+} from "@/components/ui/card";
+type HospitalProps = {
+	name: string;
+	postal_code: string;
+	address: string;
+};
+export const Hospital = (props: HospitalProps) => {
+	return (
+		<div className="container mx-auto mt-8 max-w-4xl">
+			<Card>
+				<CardHeader>
+					<CardTitle>{props.name}</CardTitle>
+				</CardHeader>
+				<CardContent>
+					<div>
+						{/* <CardDescription>{props.address}</CardDescription> */}
+						<div className="mt-4">
+							<div className="flex justify-between">
+								<div>
+									<p className="text-sm">{props.postal_code}</p>
+									<p className="font-medium">{props.address}</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</CardContent>
+			</Card>
+		</div>
+	);
+};
---
+++ app/routes/hospitals.tsx
@@ -0,0 +1,28 @@
+import { Hospital } from "@/components/hospital";
+const Hospitals = () => {
+	const hospitals = [
+		{
+			name: "医療法人つとむ会 澤田内科医院",
+			postal_code: "530-0001",
+			address: "大阪府大阪市北区梅田１―２―２―２００",
+		},
+		{
+			name: "医療法人渡辺医学会桜橋渡辺病院",
+			postal_code: "530-0001",
+			address: "大阪府大阪市北区梅田２―４―３２",
+		},
+	];
+	return (
+		<>
+			{hospitals.map((hospital) => (
+				<Hospital
+					key={hospital.name}
+					name={hospital.name}
+					postal_code={hospital.postal_code}
+					address={hospital.address}
+				/>
+			))}
+		</>
+	);
+};
+export default Hospitals;
