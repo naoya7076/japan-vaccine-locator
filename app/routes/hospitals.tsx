67e882fd4ed5a9207b0f7c6acd3f713b5420fe54
naoya7076 <naoya7076@gmail.com>
2024-03-20 14:00:46 +0900

    病院の表示件数を指定

--- app/routes/hospitals.tsx
+++ app/routes/hospitals.tsx
@@ -4,10 +4,15 @@ import { supabaseClient } from "@/lib/supabase";
 import type { LoaderFunctionArgs } from "@remix-run/cloudflare";
 import { useLoaderData } from "@remix-run/react";
 export const loader = async ({ request }: LoaderFunctionArgs) => {
+	const url = new URL(request.url);
+	const page = Number(url.searchParams.get("page")) || 0;
+	const limit = 2;
 	const response = new Response();
 	const { data: hospitals } = await supabaseClient(request, response)
 		.from("hospitals")
-		.select("id, name, postal_code, address, vaccines(name)");
+		.select("id, name, postal_code, address, vaccines(name)")
+		.limit(limit)
+		.range(page, page + limit - 1);
 
 	return json(
 		{ hospitals },
